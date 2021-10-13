795816966929daaab020c29ed1da7e61b781099b
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-10-13 00:14:36 -0400

    Upload front end

---
+++ src/forms/Login.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import logo from '../assets/logo.svg';
+
+import { URL } from "../utils/backend";
+
+import Loading from "../components/Loading";
+
+const initialUserData = {
+    userName: "",
+    userPassword: "",
+  };
+
+const Login = (props) => {
+
+    const { myHooks } = props.myProps;
+
+    const {push} = useHistory();
+
+    const [userData, setUserData] = useState(initialUserData);
+    const [validated, setValidated] = useState(false);
+
+    const submitHandler = (event) => {
+        const form = event.currentTarget;
+        event.preventDefault();
+
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+          } else {
+            myHooks.setIsLoading(true);
+            axios
+            .post(`${URL}user.login.check`, userData)
+            .then((res) => {
+                const data = res.data;
+                console.log(data);
+                if (data !== "Wrong credentials") {
+                    const user = {
+                        userId: data.userId,
+                        userName: data.userName,
+                        userFirstName: data.userFirstName,
+                        userLastName: data.userLastName,
+                        userEmail: data.userEmail,
+                        userRoleId: data.userRoleId
+                    };
+                    myHooks.setUser(user);
+                    myHooks.setWrongCred(false);
+                    myHooks.setIsLoading(false);
+                    push('/dashboard');
+                } else {
+                    myHooks.setWrongCred(true);
+                    myHooks.setIsLoading(false);
+                    push('/');
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                myHooks.setIsLoading(false);
+            });
+          }
+
+        setValidated(true);
+    }
+
+    const changeHandler = (event) => {
+        const {name, value} = event.target;
+        setUserData({...userData, [name]: value});
+    }
+
+    const onSignUpClick = (event) => {
+        event.preventDefault();
+        push('/signup');
+    }
+
+    return(
+        <div>
+            {myHooks.isLoading ? <Loading/> :
+                <Container className="form-signin">
+                    <Form onSubmit={submitHandler} noValidate validated={validated}>
+                        <img className="mb-4" src={logo} alt="logo" width="72" height="93" />
+                        <h1 className="h3 mb-3 fw-normal">Please sign in:</h1>
+                        <Form.Group className="mb-3">
+                            <Form.Control type="text" name="userName" placeholder="Enter User Name" value={userData.userName} onChange={changeHandler}  required/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Control type="password" name="userPassword" placeholder="Enter Password" value={userData.userPassword} onChange={changeHandler} required/>
+                        </Form.Group>
+                        {myHooks.wrongCred ? <p className="errors">Wrong username and/or password</p> : null}
+                        <hr className="my-4"/>
+                        <Container className="buttons">
+                            <Button variant="primary" type="submit">
+                                Sign In
+                            </Button>
+                            <Button variant="secondary" onClick={onSignUpClick}>
+                                Sign up
+                            </Button>
+                        </Container>
+                    </Form>
+                </Container>
+            }
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/forms/SingUp.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import logo from '../assets/logo.svg';
+import { URL } from "../utils/backend";
+import { useHistory } from "react-router";
+
+import Loading from "../components/Loading";
+
+const initialUserData = {
+    userName: "",
+    userPassword: "",
+    userFirstName: "",
+    userLastName: "",
+    userEmail: "",
+    userRoleId: 1
+  };
+
+
+const SignUp = (props) => {
+
+    const {myHooks} = props;
+
+    const {push} = useHistory();
+
+    const [validated, setValidated] = useState(false);
+    const [userData, setUserData] = useState(initialUserData);
+
+    const submitHandler = (event) => {
+        const form = event.currentTarget;
+        event.preventDefault();
+
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+          } else {
+            myHooks.setIsLoading(true);
+            axios
+            .post(`${URL}user.signup.check`, userData)
+            .then((res) => {
+                const data = res.data;
+                console.log(data);
+                if (data !== "Wrong credentials") {
+                    myHooks.setWrongCred(false);
+                    myHooks.setIsLoading(false);
+                    push('/');
+                } else {
+                    myHooks.setWrongCred(true);
+                    myHooks.setIsLoading(false);
+                    push('/signup');
+                }
+                
+            })
+            .catch((err) => {
+                console.log(err);
+                myHooks.setIsLoading(false);
+            });
+          }
+
+        setValidated(true);
+    }
+
+    const changeHandler = (event) => {
+        const {name, value} = event.target;
+        setUserData({...userData, [name]: value});
+    }
+
+    const onBackClick = (event) => {
+        event.preventDefault();
+        push('/');
+    }
+
+    return (
+        <div>
+            {myHooks.isLoading ? <Loading/> :
+                <Container className="form-signup">
+                    <Container className="py-5 text-center" >
+                        <img className="d-block mx-auto mb-4" src={logo} alt="logo" width="72" height="93"/>
+                        <h2>Sign up form</h2>
+                        <p className="lead">Please enter yours personal information</p>
+                    </Container>
+                    <Container className="row g-5">
+                        <Container className="col-md-12 col-lg-12">
+                            <h4 className="mb-3">Personal information</h4>
+                            <Form onSubmit={submitHandler} noValidate validated={validated}>
+                                <Container className="row g-3">            
+                                    <Form.Group className="col-sm-6">
+                                        <Form.Label className="form-label">
+                                            First name:
+                                        </Form.Label>
+                                        <Form.Control type="text" name="userFirstName" placeholder="First Name" value={userData.userFirstName} onChange={changeHandler}  required/>
+                                    </Form.Group>
+                                    <Form.Group className="col-sm-6">
+                                        <Form.Label className="form-label">
+                                            Last name:
+                                        </Form.Label>
+                                        <Form.Control type="text" name="userLastName" placeholder="Last Name" value={userData.userLastName} onChange={changeHandler}  required/>
+                                    </Form.Group>
+                                    <Form.Group className="col-12">
+                                        <Form.Label className="form-label">
+                                            User name:
+                                        </Form.Label>
+                                        <Form.Control type="text" name="userName" placeholder="User Name" value={userData.userName} onChange={changeHandler}  required/>
+                                    </Form.Group>
+                                    <Form.Group className="col-12">
+                                        <Form.Label className="form-label">
+                                            Password:
+                                        </Form.Label>
+                                        <Form.Control type="text" name="userPassword" placeholder="Password" value={userData.userPassword} onChange={changeHandler}  required/>
+                                    </Form.Group>
+                                    <Form.Group className="col-12">
+                                        <Form.Label className="form-label">
+                                            Email:
+                                        </Form.Label>
+                                        <Form.Control type="email" name="userEmail" placeholder="email@example.com" value={userData.userEmail} onChange={changeHandler}  required/>
+                                    </Form.Group>
+                                </Container>
+                                {myHooks.wrongCred ? <p className="errors">User Name already exist</p> : null}
+                                <hr className="my-4"/>
+                                <Container className="buttons">
+                                    <Button variant="primary" type="submit">
+                                        Sign Up
+                                    </Button>
+                                    <Button variant="secondary" onClick={onBackClick}>
+                                        Back to Login
+                                    </Button>
+                                </Container>
+                            </Form>
+                        </Container> 
+                    </Container>
+                </Container>
+            }
+        </div>);
+}
+
+export default SignUp;
